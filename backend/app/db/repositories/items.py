edeb974d774deb372d683c15602c97dbb122d197
Ed Baker <edwardrbaker@gmail.com>
2022-10-09 04:06:30 +0000

    fix pypika filter to check if a string contains the search string

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -142,9 +142,11 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         # fmt: on
 
         if title:
+            query_params.append('%' + title + '%')
+            query_params_count += 1
 
             # fmt: off
-            query = query.where(items.title == title ).select(items)
+            query = query.where(items.title.like(Parameter(query_params_count))).select(items)
             # fmt: on
 
         if tag:
